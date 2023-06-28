8e223d9bbe2c206c36018ec0df08e1a2ed425fd2
Paula Rodrigues <pullynnhah@icloud.com>
2023-06-28 13:17:33 -0300

    feat: implement schema validation
    Took 14 minutes

--- src/middlewares/validate.middleware.ts
+++ src/middlewares/validate.middleware.ts
@@ -1,13 +1,15 @@
 import { NextFunction, Request, Response } from "express";
 import { validationError } from "../errors/error";
 
-const validate = schema => (req: Request, res: Response, next: NextFunction) => {
-  const validation = schema.validate(req.body, { abortEarly: false });
-  if (validation.error) {
-    const errors = validation.error.details.map(err => err.message);
-    console.log(errors);
-    throw validationError(errors);
-  } else next();
-};
+const validate =
+  schema =>
+  (req: Request, res: Response, next: NextFunction): void => {
+    const validation = schema.validate(req.body, { abortEarly: false });
+    if (validation.error) {
+      const errors = validation.error.details.map(err => err.message);
+      console.log(errors);
+      throw validationError(errors);
+    } else next();
+  };
 
-export default validate;
+export { validate };
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { authController } from "../controllers/auth.controller";
-import validate from "../middlewares/validate.middleware";
+import { validate } from "../middlewares/validate.middleware";
 import { authSchema } from "../schemas/auth.schema";
 
 const router = Router();
